1724de0675c2239fa503b04e9b940ad433175e15
Sangathamizhan1810 <sangathamizh07@gmail.com>
2025-09-16 22:19:53 +0530

    minimized project

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const fullStackprojectData = [
   {
     projectName: "E-commerce website",
-    projectUrl: "https://sangathamizh22.ccbp.tech",
+    projectUrl: "https://sangathamizhan1810.github.io/4wFrontend/",
     technologiesUsed: ["HTML", "CSS","React","JavaScript","API"],
     description: " A responsive online shopping interface with product listings, filters, and cart functionality. Developed using React for a seamless, modular shopping experience."
   },{
@@ -26,96 +26,11 @@ const fullStackprojectData = [
     technologiesUsed: ["HTML", "CSS","JavaScript","API"],
     description: " A functional Wikipedia clone built using HTML, CSS, and JavaScript, leveraging the fetch() method with a GET request to retrieve article data dynamically from the Wikipedia API. This project demonstrates real-time content rendering based on user search input, offering a simplified yet powerful search experience."
   },  
-  {
-    projectName: "Google Search bar Clone",
-    projectUrl: "https://sangathamizhan9.ccbp.tech",
-    technologiesUsed: ["HTML", "CSS","React","JavaScript"],
-    description: "A sleek, responsive clone of Google’s search bar interface built using React. Mimics real-time input behavior with clean styling and a simple user experience."
-  },
-  {
-    projectName: "Spotify Song Downloader with Rapid API Integration Website",
-    projectUrl: "https://sangathamizhan7.ccbp.tech",
-    technologiesUsed: ["HTML", "CSS", "JavaScript","API"],
-    description: "A web-based Spotify Song Downloader built using HTML, CSS, and JavaScript, integrated with the Rapid API platform. This project demonstrates fetching music data through a demo-purchased API, allowing users to search and access song details dynamically."
-  },
-  {
-    projectName: "Multi-Page Game Store Website",
-    projectUrl: "https://sangathamizhan4.ccbp.tech",
-    technologiesUsed: ["HTML", "CSS"],
-    description: " A visually engaging game store website built using HTML and CSS, featuring multiple pages like home, product listings, and game details. Designed with responsive layouts and clean navigation to simulate a real-world digital storefront experience."
-  },
-  
-  {
-      projectName: "History Delete page",
-      projectUrl: "https://sangareact3.ccbp.tech",
-      technologiesUsed: ["HTML", "CSS", "JavaScript","React"],
-      description: "A responsive History Search and Delete page built using JSX and styled with CSS, allowing users to filter and remove items dynamically from a list.."
-    },
     {
-      projectName: "Emoji Game",
-      projectUrl: "https://sangareact5.ccbp.tech",
-      technologiesUsed: ["HTML", "CSS", "JavaScript","React"],
-      description: "A fun and interactive Emoji Game built using JSX with custom CSS styling, featuring multiple pages for smooth navigation and gameplay experience."
-    },{
-    projectName: "Coin toss game",
-    projectUrl: "https://sangathamizh13.ccbp.tech",
-    technologiesUsed: ["HTML", "CSS","React","JavaScript"],
-    description: " A fun, interactive coin toss simulator with real-time flipping animation and score tracking. Crafted with React to update results instantly on each toss."
-  },
-    {
-      projectName: "Live Clock with Toggle Button",
-      projectUrl: "https://sangareact4.ccbp.tech",
-      technologiesUsed: ["HTML", "CSS", "JavaScript","React"],
-      description: "A live clock display that updates in real-time, built using JSX and styled with CSS, featuring a Hide/Show button to toggle the clock’s visibility for an interactive user experience."
-    },{
       projectName: "Appointment Booking and List Display",
       projectUrl: "https://sangareact2.ccbp.tech",
       technologiesUsed: ["HTML", "CSS", "JavaScript","React"],
       description: "An Appointment Booking page built using JSX and styled with CSS, allowing users to schedule appointments. The page dynamically renders a list of scheduled appointments, providing a clear and organized view of upcoming events."
-    },
-    {
-      projectName: "Dynamic Comment Section App",
-      projectUrl: "https://sangareaact1.ccbp.tech",
-      technologiesUsed: ["HTML", "CSS", "JavaScript","React"],
-      description: "A simple and interactive comment section built using JSX and styled with CSS, where each user input is added and displayed instantly below the form. This app provides a smooth user experience for adding and viewing multiple comments in real-time."
-    },{
-      projectName: "Static Todo List with Delete Functionality",
-      projectUrl: "https://sangatodos.ccbp.tech",
-      technologiesUsed: ["HTML", "CSS", "JavaScript","React"],
-      description: "A preset Todo application created using JSX and CSS, where a predefined list of tasks is displayed. Users can remove individual tasks using the functional delete icon, while other actions are visually present but inactive."
-    },
-    {
-      projectName: "API Status Viewer using Gorest API",
-      projectUrl: "https://sangathamizhan3.ccbp.tech",
-      technologiesUsed: ["HTML", "CSS","JavaScript","API"],
-      description: "A front-end tool built to visually monitor API response status by integrating with the Gorest API. Developed using clean UI logic, it dynamically reflects API responses, helping users understand whether the service is active, responsive, or facing issues."
-    },{
-      projectName: "Basic Chatbot with Preset Responses",
-      projectUrl: "https://sangachatbot.ccbp.tech/",
-      technologiesUsed: ["HTML", "CSS", "JavaScript"],
-      description: "A simple chatbot built using HTML, CSS, and JavaScript, designed to simulate a conversation with preset replies. It uses basic input handling to return predefined responses, offering a straightforward intro to chatbot UI and front-end interactivity."
-    },
-    {
-      projectName: "Random Word Cloud Generator",
-      projectUrl: "https://sangacloudwords.ccbp.tech",
-      technologiesUsed: ["HTML","CSS" , "JavaScript"],
-      description: "A creative word cloud generator built using HTML, CSS, and JavaScript, where each word appears in random sizes to mimic a cloud-like formation. This project showcases dynamic styling and layout generation, offering a visually engaging way to display text elements."
-    },{
-      projectName: "Simple Image Resizer with Conditional Rendering",
-      projectUrl: "https://sangaimagresize.ccbp.tech",
-      technologiesUsed: ["HTML","CSS" , "JavaScript"],
-      description: "An interactive image resizer built using HTML and CSS, with basic JavaScript logic to render different image sizes based on simple conditions. The project demonstrates dynamic style manipulation and responsive design through user-triggered input."
-    },
-    {
-      projectName: "Simple Traffic Light with Conditional Logic",
-      projectUrl: "https://sangatraffic.ccbp.tech",
-      technologiesUsed: ["HTML","CSS" , "JavaScript"],
-      description: "A basic traffic light simulation built using HTML, CSS, and JavaScript, where the light changes color based on simple conditional logic."
-    },{
-      projectName: "Multi-Website Layout on a Single Page",
-      projectUrl: "https://sangaallinone.ccbp.tech",
-      technologiesUsed: ["HTML", "CSS"],
-      description: "A single-page layout showcasing four distinct static websites using HTML and CSS. Each section displays a unique, self-contained website layout, making the page interactive and visually engaging, while demonstrating effective use of multiple styles and content sections."
     },
     {
       projectName: "To-do list",
